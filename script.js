47557d3774149c77443bf2146e4a1db955a9bb16
easrng <easrng@gmail.com>
2021-10-08 17:11:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-import beacon from "https://easrng.github.io/stats-control/stats.js"
 import esmBundler from "./bundler.js";
-beacon("page-load")
+stats.beacon("page-load")
 document.forms[0].addEventListener("submit", async e => {
   e.preventDefault();
   window.output.textContent = "Bundling...";
   window.btn.disabled = true;
-  beacon("bundle-start")
+  stats.beacon("bundle-start")
   try {
     window.output.textContent = await esmBundler({
       modules: Object.entries(JSON.parse(document.forms[0].mods.value)).map(
@@ -14,12 +13,12 @@ document.forms[0].addEventListener("submit", async e => {
       importMap: JSON.parse(document.forms[0].im.value)
     });
     window.btn.disabled = false;
-    beacon("bundle-finish")
+    stats.beacon("bundle-finish")
   } catch (e) {
     console.error(e);
     window.btn.disabled = false;
     window.output.textContent =
       "Error bundling. Make sure the modules are served with CORS headers.";
-    beacon("bundle-error")
+    stats.beacon("bundle-error")
   }
 });
